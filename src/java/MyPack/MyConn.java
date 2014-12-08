dccb13ba29370aeb792c5581ba53bfd423ca8dfa
yashdeep <yashdeeph709@gmail.com>
2014-12-08 20:22:33 +0530

    done running again

--- src/java/MyPack/MyConn.java
+++ src/java/MyPack/MyConn.java
@@ -1,18 +1,24 @@
 package MyPack;
 import java.sql.*;
-  
+ 
 public class MyConn{
-public static Connection con;
-
+    public static Connection con;
     public static Connection getMySqlCon() {
         try{
-    if(con==null){
-          Class.forName("com.mysql.jdbc.Driver");
-          con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lifecare","root",""); 
-      }}
-    catch(Exception ex){
-        ex.printStackTrace();
+            if(con==null){
+            String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+            String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+            String username=System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            String password=System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+            
+            String url = String.format("jdbc:mysql://%s:%s/", host, port);
+
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);      }
+            }
+            catch(Exception ex){
+            ex.printStackTrace();
+            }
+            return con;
         }
-         return con;
-    }
 }
