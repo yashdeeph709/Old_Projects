310d7bcfcef19b86818c7737cec88e708eb1dcab
yashdeep <yashdeeph709@gmail.com>
2014-12-08 19:16:27 +0530

    done running again

--- src/java/MyPack/MyConn.java
+++ src/java/MyPack/MyConn.java
@@ -1,35 +1,18 @@
-package MyPack;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
+package com;
+import java.sql.*;
+  
 public class MyConn{
-private static MyConn connector_ = null;
-
-public static MyConn getInstance() throws Exception {
-if(connector_ == null) {
-connector_ = new MyConn();
-}
-return connector_;
-}
-
-    public static Connection getMySqlCon() throws SQLException {
-        Connection c=null;
-    try {
-        Context ctx = new InitialContext();
-        DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/lifecare");
-        c = ds.getConnection();
-        return c;
-    } catch (NamingException ex) {
-        Logger.getLogger(MyConn.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    return c;
+public static Connection con;
+public static Connection getcon()
+{
+try{
+    if(con==null){
+          Class.forName("com.mysql.jdbc.Driver");
+          con=DriverManager.getConnection("jdbc:mysql://localhost:3306/callit","root","root"); 
+      }}
+    catch(Exception ex){
+        ex.printStackTrace();
+        }
+         return con;
     }
 }
