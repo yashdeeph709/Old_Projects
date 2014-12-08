0950d9ee952f6e51b2861b34bbe20d89c65ffbf2
yashdeep <yashdeeph709@gmail.com>
2014-12-08 18:58:28 +0530

    done running again

--- src/java/MyPack/MyConn.java
+++ src/java/MyPack/MyConn.java
@@ -3,8 +3,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class MyConn{
@@ -17,10 +20,16 @@ connector_ = new MyConn();
 return connector_;
 }
 
-public static Connection getConnection() throws Exception {
-Context ctx = new InitialContext();
-DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/lifecare");
-Connection c = ds.getConnection();
-return c;
-}
+    public static Connection getMySqlCon() throws SQLException {
+        Connection c=null;
+    try {
+        Context ctx = new InitialContext();
+        DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/lifecare");
+        c = ds.getConnection();
+        return c;
+    } catch (NamingException ex) {
+        Logger.getLogger(MyConn.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return c;
+    }
 }
